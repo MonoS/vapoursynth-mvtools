685986fb53f1d4fa0bd174984b80ff40cb191301
MonoS94 <MonoS94@gmail.com>
2015-12-12 23:57:37 +0100

    Add AVX2 implementation of ToPixels only for 16bit input.
    Compiled in O2 march corei7-avx2 with GCC 5.1.0 x64 this give a 4.6x speedup to the C version.
    Compiled in O3 [etc...] give a 1.2x speedup to the C version [cause O3 make the C version a lot faster].
    
    For test code and results go here: http://pastebin.com/LtP3m4Mp

--- src/Overlap.h
+++ src/Overlap.h
@@ -130,5 +130,42 @@ void ToPixels(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, int nSrcPitch
     }
 }
 
+__inline __m256i _mm256_loadu2_m128i(__m128i *low, __m128i *high)
+{
+    return _mm256_inserti128_si256(_mm256_castsi128_si256(_mm_loadu_si128((__m128i*)low)),
+                                                          _mm_loadu_si128((__m128i*)high),1);
+
+}
+
+void ToPixels_AVX2_16bit(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, int nSrcPitch, int nWidth, int nHeight, int bitsPerSample)
+{
+	static __m256i const sixteen  = _mm256_set1_epi32(16);
+	static __m128i const five     = _mm_set1_epi64x(5);
+    for (int h=0; h<nHeight; h++)
+    {
+		const uint32_t *pSrc = (const uint32_t *)pSrc8;
+        uint16_t *pDst = (uint16_t *)pDst8;
+
+        nWidth_16 = (nWidth & ~15);
+        for (int i = 0; i < nWidth_16; i += 16)
+        {
+            __m256i src_1 = _mm256_loadu2_m128i((__m128i* )(pSrc + i), (__m128i* )(pSrc + i + 8));
+            __m256i src_2 = _mm256_loadu2_m128i((__m128i* )(pSrc + i + 4), (__m128i* )(pSrc + i + 12));
+
+			__m256i a1 = _mm256_srl_epi32(_mm256_add_epi32(src_1, sixteen), five);
+			__m256i a2 = _mm256_srl_epi32(_mm256_add_epi32(src_2, sixteen), five);
+			_mm256_storeu_si256((__m256i* )(pDst + i), _mm256_packus_epi32(a1, a2));
+
+        }
+
+        for (int i = nWidth_16; nWidth_16 < nWidth; i++)
+        {
+            int a = (pSrc[i] + 16)>>5;
+            pDst[i] = min(pixelMax, a);
+        }
+        pDst8 += nDstPitch;
+        pSrc8 += nSrcPitch;
+    }
+}
 
 #endif
