8c604506147c5f9db0e7691cbaa3b40b7dc92e8f
MonoS94 <MonoS94@gmail.com>
2015-12-13 15:31:42 +0100

    Who wrote this code?? The shift is by an immediate

--- src/Overlap.h
+++ src/Overlap.h
@@ -140,7 +140,6 @@ __inline __m256i _mm256_loadu2_m128i(__m128i *low, __m128i *high)
 void ToPixels_AVX2_16bit(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, int nSrcPitch, int nWidth, int nHeight, int bitsPerSample)
 {
 	static __m256i const sixteen  = _mm256_set1_epi32(16);
-	static __m128i const five     = _mm_set1_epi64x(5);
 	static int const pixelMax     = 65535;
 	
     for (int h=0; h<nHeight; h++)
@@ -154,8 +153,8 @@ void ToPixels_AVX2_16bit(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, in
             __m256i src_1 = _mm256_loadu2_m128i((__m128i* )(pSrc + i), (__m128i* )(pSrc + i + 8));
             __m256i src_2 = _mm256_loadu2_m128i((__m128i* )(pSrc + i + 4), (__m128i* )(pSrc + i + 12));
 
-			__m256i a1 = _mm256_srl_epi32(_mm256_add_epi32(src_1, sixteen), five);
-			__m256i a2 = _mm256_srl_epi32(_mm256_add_epi32(src_2, sixteen), five);
+			__m256i a1 = _mm256_srli_epi32(_mm256_add_epi32(src_1, sixteen), 5);
+			__m256i a2 = _mm256_srli_epi32(_mm256_add_epi32(src_2, sixteen), 5);
 			_mm256_storeu_si256((__m256i* )(pDst + i), _mm256_packus_epi32(a1, a2));
 
         }
@@ -173,7 +172,6 @@ void ToPixels_AVX2_16bit(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, in
 void ToPixels_SSE2_16bit(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, int nSrcPitch, int nWidth, int nHeight, int bitsPerSample)
 {
 	static __m128i const sixteen  = _mm_set1_epi32(16);
-	static __m128i const five     = _mm_set1_epi64x(5);
 	static int const pixelMax     = 65535;
 	
     for (int h=0; h<nHeight; h++)
@@ -187,8 +185,8 @@ void ToPixels_SSE2_16bit(uint8_t *pDst8, int nDstPitch, const uint8_t *pSrc8, in
             __m128i src_1 = _mm_lddqu_si128((__m128i* )(pSrc + i));
             __m128i src_2 = _mm_lddqu_si128((__m128i* )(pSrc + i + 4));
 
-			__m128i a1 = _mm_srl_epi32(_mm_add_epi32(src_1, sixteen), five);
-			__m128i a2 = _mm_srl_epi32(_mm_add_epi32(src_2, sixteen), five);
+			__m128i a1 = _mm_srli_epi32(_mm_add_epi32(src_1, sixteen), 5);
+			__m128i a2 = _mm_srli_epi32(_mm_add_epi32(src_2, sixteen), 5);
 			_mm_storeu_si128((__m128i* )(pDst + i), _mm_packus_epi32(a1, a2));
         }
 
